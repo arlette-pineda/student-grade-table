5cf0b87edcc6782409991b0da76fe299df489e58
Arlette Pineda <arlettepimo@gmail.com>
2020-02-06 13:12:49 -0800

    Appended newTRow to tbody instead of table element; added striped rows styling.

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,19 +3,21 @@ class GradeTable{
     this.tableElement = tableElement;
   }
   updateGrades(grades){
-    this.tableElement.querySelector('#t-body').textContent = '';
+    var tbody = this.tableElement.querySelector('#t-body')
+    tbody.textContent = '';
     for(var i = 0; i < grades.length; i++){
       var newTRow = document.createElement('tr');
       var stuName = document.createElement('td');
+      stuName.setAttribute('scope', 'row');
       stuName.textContent = grades[i].name;
       var stuCourse = document.createElement('td');
       stuCourse.textContent = grades[i].course;
       var stuGrade = document.createElement('td');
       stuGrade.textContent = grades[i].grade;
       newTRow.append(stuName, stuCourse, stuGrade);
-      this.tableElement.appendChild(newTRow);
+      tbody.appendChild(newTRow);
       document.querySelector('thead').classList.add('thead-dark');
-      // this.tableElement.classList.add('table-striped');
+      this.tableElement.classList.add('table-striped');
     }
   }
 }
