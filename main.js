1f0fa9b4146d503e539d1607f7cd2589fad82e26
Arlette Pineda <arlettepimo@gmail.com>
2020-02-06 16:37:05 -0800

    Modified instan. of App and included PageHeader instan. as second argument in main.js.

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var tableElement = document.getElementById('table-el');
 var gradeTableIn = new GradeTable(tableElement);
 var theHeader = document.querySelector('header');
 var pageHeader = new PageHeader(theHeader);
-var app = new App(gradeTableIn);
+var app = new App(gradeTableIn, pageHeader);
 app.start();
