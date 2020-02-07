3f71c3284094ce2a80a74356777cacfd51377ee9
Arlette Pineda <arlettepimo@gmail.com>
2020-02-06 16:26:57 -0800

    Modified App constructor with second param named pageHeader and assigned to property of this object.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var tableElement = document.getElementById('table-el');
 var gradeTableIn = new GradeTable(tableElement);
+var theHeader = document.querySelector('header');
+var pageHeader = new PageHeader(theHeader);
 var app = new App(gradeTableIn);
 app.start();
