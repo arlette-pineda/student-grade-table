fa3054d23694ce7a9852e7f4dc987bd2f8e4cdab
Arlette Pineda <arlettepimo@gmail.com>
2020-02-07 18:27:46 -0800

    Moved dom creation to renderGradeRow method from updateGrades(); Added row with delete button.

--- components/grade-table.js
+++ components/grade-table.js
@@ -6,6 +6,15 @@ class GradeTable{
     var tbody = this.tableElement.querySelector('#t-body')
     tbody.textContent = '';
     for(var i = 0; i < grades.length; i++){
+      this.renderGradeRow;
+      this.deleteGrade;
+        //where the dom creation was here
+    }
+  }
+  onDeleteClick(deleteGrade){
+    this.deleteGrade = deleteGrade;
+  }
+  renderGradeRow(data, deleteGrade){
       var newTRow = document.createElement('tr');
       var stuName = document.createElement('td');
       stuName.setAttribute('scope', 'row');
@@ -14,10 +23,24 @@ class GradeTable{
       stuCourse.textContent = grades[i].course;
       var stuGrade = document.createElement('td');
       stuGrade.textContent = grades[i].grade;
-      newTRow.append(stuName, stuCourse, stuGrade);
+      var stuOperation = document.createElement('td');
+      var tDBut = document.createElement('button');
+      tDBut.setAttribute('type', 'button');
+      tDBut.setAttribute('class', 'btn, btn-danger');
+      tDBut.textContent = 'DELETE';
+      tDBut.addEventListener('click', function(){
+        deleteGrade(data.id);
+      });
+      stuOperation.append(tDBut);
+      newTRow.append(stuName, stuCourse, stuGrade, stuOperation);
       tbody.appendChild(newTRow);
       document.querySelector('thead').classList.add('thead-dark');
       this.tableElement.classList.add('table-striped');
-    }
   }
 }
+
+
+//
+// var tableRow = document.createElement('tr');
+// var tabDName = document.createElement('td');
+// var tabDCourse = document.createElement('td');
